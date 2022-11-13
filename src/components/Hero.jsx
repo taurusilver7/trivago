eeb604a9a7d46af2cb388ef8e335d3132b7365bd
Devi Prasad Palakollu <prasad20697@gmail.com>
2022-11-13 22:32:52 +0530

    hero component init

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Hero = () => {
+	return (
+		<div>
+			<div>
+				<div>
+					<h1>It's a Giant world Out</h1>
+					<h1>Go, Explore</h1>
+					<p>
+						We provied your dream place. We make our curstomers satisfied
+						by providing multiple choices.
+					</p>
+					<div>
+						<button type="button">Get Started</button>
+						<button type="button">Get Demo</button>
+					</div>
+				</div>
+				<div>
+					<img src="" alt="dashboard-img" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;

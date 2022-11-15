0418bc4198fd5787bd9286edc2e9c12e3ea091ae
Devi Prasad Palakollu <prasad20697@gmail.com>
2022-11-15 11:57:17 +0530

    Hero section modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 
-const Hero = () => {
+const Hero = ({ hero: { title, subtitle, btn1, btn2, text, img } }) => {
 	return (
-		<div>
-			<div>
-				<div>
-					<h1>It's a Giant world Out</h1>
-					<h1>Go, Explore</h1>
-					<p>
-						We provied your dream place. We make our curstomers satisfied
-						by providing multiple choices.
-					</p>
-					<div>
-						<button type="button">Get Started</button>
-						<button type="button">Get Demo</button>
+		<>
+			<div className="flex flex-col bg-gradient-to-b from-emerald-200 to-white h-auto w-auto">
+				<div className="trivago-container grid items-start justify-items-center">
+					<div className="grid items-center justify-items-center mt-36 mb-16 md:mt-28 md:mb-12">
+						<h1 className="nav-heading">{title}</h1>
+						<h1 className="nav-heading">{subtitle}</h1>
+						<p className="text-base text-center my-5 sm:text-sm">
+							{text}
+						</p>
+						<div className="flex items-center justify-center gap-11 sm:gap-3 sm:flex-col sm:w-full">
+							<button type="button" className="btn button-emerald">
+								{btn1}
+							</button>
+							<button type="button" className="btn button-light">
+								{btn2}
+							</button>
+						</div>
+					</div>
+					<div className="flex items-center justify-center mb-11">
+						<img
+							src={img}
+							className="h-[85vh] w-full object-fill lg:h-[80vh] md:h-[57vh] sm:h-[35vh] sm:object-contain drop-shadow-emerald"
+							alt="dashboard-img"
+						/>
 					</div>
 				</div>
-				<div>
-					<img src="" alt="dashboard-img" />
-				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../images/logo2.png";
 
 const Navbar = () => {
-	return <div></div>;
+	return (
+		<header>
+			<nav>
+				<NavLink to={"/"} className="">
+					<img src={logo} alt="logo/img" className="" />
+				</NavLink>
+				<ul>
+					<li>
+						<NavLink to={"#"}>Home</NavLink>
+					</li>
+				</ul>
+
+				<ul>
+					<li><button type="button">Join us</button></li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Navbar;

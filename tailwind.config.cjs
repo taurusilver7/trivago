0e860f3f5559187fb0c94b3880323bf65ba91cb4
Devi Prasad Palakollu <prasad20697@gmail.com>
2022-11-15 11:57:01 +0530

    public & config modified

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
+		screens: {
+			xl: { max: "1200px" },
+			lg: { max: "991px" },
+			md: { max: "767px" },
+			sm: { max: "550px" },
+			xs: { max: "425px" },
+		},
 	},
 	plugins: [],
 };

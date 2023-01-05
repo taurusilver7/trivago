d9cb9b4f6f6dd1f1ebc3240f0930144401e3974e
Devi Prasad Palakollu <prasad20697@gmail.com>
2023-01-06 01:03:26 +0530

    app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Advertise from "./components/Advertise";
+import Banner from "./components/Banner";
+import Explore from "./components/Explore";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Memory from "./components/Memory";
 import Navbar from "./components/Navbar";
-import { hero, navlinks, memory } from "./data/travigo-data";
+import Newsletter from "./components/Newsletter";
+import Pricings from "./components/Pricings";
+import {
+	hero,
+	navlinks,
+	memory,
+	placesAPI,
+	brands,
+	pricingapi,
+	bannerAPI,
+	footerAPI,
+} from "./data/travigo-data";
 
 function App() {
 	return (
@@ -11,7 +25,12 @@ function App() {
 			<Navbar navlinks={navlinks} />
 			<Hero hero={hero} />
 			<Memory memory={memory} />
-			<Footer />
+			<Explore title="Explore the Beauty of World" placesAPI={placesAPI} />
+			<Advertise brands={brands} />
+			<Pricings pricingAPI={pricingapi} />
+			<Banner bannerAPI={bannerAPI} />
+			<Newsletter />
+			<Footer footerAPI={footerAPI} />
 		</div>
 	);
 }
